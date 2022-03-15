408cf39144781fd6844350f4a52158a706b44cd0
WooJin Park <ak2j38@gmail.com>
2022-03-15 16:55:55 +0900

    feature : 1_1 코드 구현
    1. 책의 예제인 User 대신 현재 미션과 비슷한 Article로 수정
    2. mysql 대신 간단한 h2 DB 사용
    
    end

--- src/main/java/org/ader/tobyspringstudy/TobyspringstudyApplication.java
+++ src/main/java/org/ader/tobyspringstudy/TobyspringstudyApplication.java
@@ -1,13 +1,37 @@
 package org.ader.tobyspringstudy;
 
+import org.ader.tobyspringstudy.dao.ArticleDao;
+import org.ader.tobyspringstudy.domain.Article;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import java.sql.SQLException;
+import java.time.LocalDateTime;
 
 @SpringBootApplication
 public class TobyspringstudyApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(TobyspringstudyApplication.class, args);
+	public static void main(String[] args) throws ClassNotFoundException, SQLException {
+		//SpringApplication.run(TobyspringstudyApplication.class, args);
+
+		ArticleDao articleDao = new ArticleDao();
+
+		Article article = new Article();
+		article.setId(1L);
+		article.setWriter("ader");
+		article.setTitle("title test");
+		article.setContents("contents test");
+		article.setCreatedTime(LocalDateTime.now());
+		article.setUpdatedTime(LocalDateTime.now());
+
+		articleDao.add(article);
+		System.out.println("등록 성공");
+
+		Article findArticle = articleDao.get(article.getId());
+		System.out.println("findArticle.getWriter() = " + findArticle.getWriter());
+		System.out.println("findArticle.getTitle() = " + findArticle.getTitle());
+
+		System.out.println(findArticle.getId() + " 조회 성공");
 	}
 
 }
---
+++ src/main/java/org/ader/tobyspringstudy/dao/ArticleDao.java
@@ -0,0 +1,61 @@
+package org.ader.tobyspringstudy.dao;
+
+import org.ader.tobyspringstudy.domain.Article;
+
+import java.sql.*;
+import java.time.LocalDateTime;
+
+/**
+ * Ver 1_1 문제점
+ *      1. 중복코드가 너무 많다. (Driver 가져오기, DB 커넥션 연결)
+ *      2. main에서 테스트를 진행하고 있다.
+ */
+public class ArticleDao {
+
+    public void add(Article article) throws ClassNotFoundException, SQLException {
+        Class.forName("org.h2.Driver");
+        Connection conn = DriverManager.getConnection("jdbc:h2:tcp://localhost/~/toby_spring", "sa", "");
+
+        PreparedStatement pstmt = conn.prepareStatement("INSERT INTO article(id, writer, title, contents, created_time, updated_time) " +
+                "VALUES (?, ?, ?, ?, ?, ?) ");
+
+        pstmt.setLong(1, article.getId());
+        pstmt.setString(2, article.getWriter());
+        pstmt.setString(3, article.getTitle());
+        pstmt.setString(4, article.getContents());
+        pstmt.setObject(5, article.getCreatedTime());
+        pstmt.setObject(6, article.getUpdatedTime());
+
+        pstmt.executeUpdate();
+
+        pstmt.close();
+        conn.close();
+    }
+
+    public Article get(Long id) throws ClassNotFoundException, SQLException {
+        Class.forName("org.h2.Driver");
+        Connection conn = DriverManager.getConnection("jdbc:h2:tcp://localhost/~/toby_spring", "sa", "");
+
+        PreparedStatement pstmt = conn.prepareStatement("SELECT * " +
+                "FROM article " +
+                "WHERE id = ?");
+        pstmt.setLong(1, id);
+
+        ResultSet rs = pstmt.executeQuery();
+        rs.next();
+        Article article = new Article();
+        article.setId(rs.getLong("id"));
+        article.setWriter(rs.getString("writer"));
+        article.setTitle(rs.getString("title"));
+        article.setContents(rs.getString("contents"));
+        article.setCreatedTime((LocalDateTime)rs.getObject("created_time"));
+        article.setUpdatedTime((LocalDateTime)rs.getObject("updated_time"));
+
+        rs.close();
+        pstmt.close();
+        conn.close();
+
+        return article;
+    }
+}
+
